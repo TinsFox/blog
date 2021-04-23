7e8674886a9e2c7588da7727e8b3942991722f25
TinsFox <fox@tinsfox.com>
2021-04-23 17:07:02 +0800

    修改默认语言

--- docs/.vuepress/config/pluginConf.js
+++ docs/.vuepress/config/pluginConf.js
@@ -3,6 +3,12 @@ const moment = require('moment');
 moment.locale("zh-cn");
 
 module.exports = {
+  locales: {
+    // 键名是该语言所属的子路径
+    // 作为特例，默认语言可以使用 '/' 作为其路径。
+    '/': {
+      lang: 'zh-cn', // 将会被设置为 <html> 的 lang 属性
+    },
   '@vuepress/pwa': {
     serviceWorker: true,
     updatePopup: {
